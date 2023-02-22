52833d0d049906519a428d64d5e415ce0d032ebc
csabbah <csabbah@ryerson.ca>
2023-02-21 23:15:10 -0500

    Polished UI

--- App.js
+++ App.js
@@ -10,6 +10,8 @@ export default function App() {
         <Text style={styles.sectionTitle}>Today's tasks</Text>
         <View style={styles.items}>
           <Task text={"Task 1"} />
+          <Task text={"Task 2"} />
+          <Task text={"Task 3"} />
         </View>
       </View>
     </View>
@@ -29,5 +31,7 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: "bold",
   },
-  items: {},
+  items: {
+    marginTop: 30,
+  },
 });
